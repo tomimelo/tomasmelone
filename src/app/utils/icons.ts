2ff1e28e9293d4ad5997d60858b0672b05772406
Tomas Melone <tomasmelone@hotmail.com>
2021-09-18 00:03:12 +0200

    feat: Add design card and avatar

--- src/app/utils/icons.ts
+++ src/app/utils/icons.ts
@@ -1,8 +1,8 @@
-import menuIcon from '@iconify-icons/codicon/menu';
-import searchIcon from '@iconify-icons/codicon/search';
-import githubIcon from '@iconify-icons/codicon/github';
-import accountIcon from '@iconify-icons/codicon/account';
-import mailIcon from '@iconify-icons/codicon/mail';
+import menu from '@iconify-icons/codicon/menu';
+import search from '@iconify-icons/codicon/search';
+import github from '@iconify-icons/codicon/github';
+import account from '@iconify-icons/codicon/account';
+import mail from '@iconify-icons/codicon/mail';
 import chevronDown from '@iconify-icons/codicon/chevron-down';
 import chevronUp from '@iconify-icons/codicon/chevron-up';
 import chevronRight from '@iconify-icons/codicon/chevron-right';
@@ -10,18 +10,20 @@ import sourceControl from '@iconify-icons/codicon/source-control';
 import heart from '@iconify-icons/codicon/heart';
 import serverProcess from '@iconify-icons/codicon/server-process';
 import tools from '@iconify-icons/codicon/tools';
+import symbolColor from '@iconify-icons/codicon/symbol-color';
 
 export const appIcons = {
-  menu: menuIcon,
-  search: searchIcon,
-  github: githubIcon,
-  account: accountIcon,
-  mail: mailIcon,
+  menu,
+  search,
+  github,
+  account,
+  mail,
   'chevron-down': chevronDown,
   'chevron-up': chevronUp,
   'chevron-right': chevronRight,
   'source-control': sourceControl,
-  'heart': heart,
+  heart,
   'server-process': serverProcess,
-  'tools': tools
+  tools,
+  'symbol-color': symbolColor
 }
